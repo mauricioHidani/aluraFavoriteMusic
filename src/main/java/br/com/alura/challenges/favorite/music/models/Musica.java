b812d67e708bf94447f5f8ce3e0e6450a57866fe
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-09-30 14:29:04 -0300

    refactor: padrao builder para os modelos

--- src/main/java/br/com/alura/challenges/favorite/music/models/Musica.java
+++ src/main/java/br/com/alura/challenges/favorite/music/models/Musica.java
@@ -19,6 +19,17 @@ public class Musica extends Audio implements Serializable {
 		this.genero = genero;
 	}
 
+	public Musica(MusicaBuilder builder) {
+		super(builder.titulo, builder.totalReproducoes, builder.totalCurtidas, builder.classificacao);
+		this.album = builder.album;
+		this.cantor = builder.cantor;
+		this.genero = builder.genero;
+	}
+
+	public static MusicaBuilder builder() {
+		return new MusicaBuilder();
+	}
+
 	public String getAlbum() {
 		return album;
 	}
@@ -29,6 +40,64 @@ public class Musica extends Audio implements Serializable {
 
 	public String getGenero() {
 		return genero;
+	}
+
+	public static class MusicaBuilder {
+		private String titulo;
+		private Integer totalReproducoes;
+		private Integer totalCurtidas;
+		private Integer classificacao;
+		private String album;
+		private String cantor;
+		private String genero;
+
+		public MusicaBuilder titulo(String titulo) {
+			this.titulo = titulo;
+			return this;
+		}
+
+		public MusicaBuilder totalReproducoes(Integer totalReproducoes) {
+			this.totalReproducoes = totalReproducoes;
+			return this;
+		}
+
+		public MusicaBuilder totalCurtidas(Integer totalCurtidas) {
+			this.totalCurtidas = totalCurtidas;
+			return this;
+		}
+
+		public MusicaBuilder classificacao(Integer classificacao) {
+			this.classificacao = classificacao;
+			return this;
+		}
+
+		public MusicaBuilder album(String album) {
+			this.album = album;
+			return this;
+		}
+
+		public MusicaBuilder cantor(String cantor) {
+			this.cantor = cantor;
+			return this;
+		}
+
+		public MusicaBuilder genero(String genero) {
+			this.genero = genero;
+			return this;
+		}
+
+
+		public Musica build() {
+			return new Musica(
+				titulo,
+				totalReproducoes,
+				totalCurtidas,
+				classificacao,
+				album,
+				cantor,
+				genero
+			);
+		}
 	}
 
 }
--- src/main/java/br/com/alura/challenges/favorite/music/models/Podcast.java
+++ src/main/java/br/com/alura/challenges/favorite/music/models/Podcast.java
@@ -17,12 +17,65 @@ public class Podcast extends Audio implements Serializable {
 		this.descricao = descricao;
 	}
 
+	public Podcast(PodcastBuilder builder) {
+		super(builder.titulo, builder.totalReproducoes, builder.totalCurtidas, builder.classificacao);
+		this.apresentador = builder.apresentador;
+		this.descricao = builder.descricao;
+	}
+
+	public static PodcastBuilder builder() {
+		return new PodcastBuilder();
+	}
+
 	public String getApresentador() {
 		return apresentador;
 	}
 
 	public String getDescricao() {
 		return descricao;
+	}
+
+	public static class PodcastBuilder {
+		private String titulo;
+		private Integer totalReproducoes;
+		private Integer totalCurtidas;
+		private Integer classificacao;
+		private String apresentador;
+		private String descricao;
+
+		public PodcastBuilder titulo(String titulo) {
+			this.titulo = titulo;
+			return this;
+		}
+
+		public PodcastBuilder totalReproducoes(Integer totalReproducoes) {
+			this.totalReproducoes = totalReproducoes;
+			return this;
+		}
+
+		public PodcastBuilder totalCurtidas(Integer totalCurtidas) {
+			this.totalCurtidas = totalCurtidas;
+			return this;
+		}
+
+		public PodcastBuilder classificacao(Integer classificacao) {
+			this.classificacao = classificacao;
+			return this;
+		}
+
+		public PodcastBuilder apresentador(String apresentador) {
+			this.apresentador = apresentador;
+			return this;
+		}
+
+		public PodcastBuilder descricao(String descricao) {
+			this.descricao = descricao;
+			return this;
+		}
+
+		public Podcast build() {
+			return new Podcast(titulo, totalReproducoes, totalCurtidas, classificacao, apresentador, descricao);
+		}
 	}
 
 }
